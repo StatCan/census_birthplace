a17a12d6390eee40a5fff2518a1ce419a9195c92
Laurent Goderre <laurent.goderre@gmail.com>
2017-09-20 15:06:42 -0400

    Added the statcan_countries library

--- src/demo.js
+++ src/demo.js
@@ -1,7 +1,8 @@
 var sgcI18nRoot = "lib/statcan_sgc/i18n/sgc/",
-  rootI18nRoot = "src/i18n/",
   countryI18nRoot = "lib/statcan_countries/i18n/",
+  rootI18nRoot = "src/i18n/",
   sgcDataUrl = "lib/statcan_sgc/sgc.json",
+  countriesDataUrl = "lib/statcan_countries/countries.json",
   birthplaceDataUrl = "data/census_birthplace.json",
   container = d3.select(".birthplace .data"),
   chart = container.append("svg")
@@ -13,15 +14,16 @@ var sgcI18nRoot = "lib/statcan_sgc/i18n/sgc/",
   showData = function() {
     chordChart(chart, settings);
   },
-  sgcData, birthplaceData;
+  sgcData, countriesData, birthplaceData;
 
 i18n.load([sgcI18nRoot, countryI18nRoot, rootI18nRoot], function() {
   d3.queue()
     .defer(d3.json, sgcDataUrl)
+    .defer(d3.json, countriesDataUrl)
     .defer(d3.json, birthplaceDataUrl)
-    .await(function(error, sgcs, birthplace) {
+    .await(function(error, sgcs, countries, birthplace) {
       sgcData = sgcs;
-      birthplaceData = birthplace;
+      countriesData = statcan_countries(countries);
 
       showData();
     });
