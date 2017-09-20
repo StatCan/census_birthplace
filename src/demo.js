d0f8bfc43df9b247b2e399406c71dd4e5448a1c4
Laurent Goderre <laurent.goderre@gmail.com>
2017-09-20 15:20:16 -0400

    Added function to process the data

--- src/demo.js
+++ src/demo.js
@@ -9,7 +9,26 @@ var sgcI18nRoot = "lib/statcan_sgc/i18n/sgc/",
     .attr("id", "canada_birthplace"),
   rootI18nNs = "census_birthplace",
   settings = {
+    getPointValue: function() {
+      return this.dataPoint.total;
+    }
+  },
+  processData = function(data) {
+    var dataLength = data.mappings.length,
+      pointsLength = data.dataPoints.length,
+      i, d, p;
 
+    for (i = 0; i < dataLength; i++) {
+      d = data.mappings[i];
+      d.valueOf = settings.getPointValue;
+      for (p = 0; p < pointsLength; p ++) {
+        if (d.dpId === data.dataPoints[p].id) {
+          d.dataPoint = data.dataPoints[p];
+        }
+      }
+    }
+
+    return data;
   },
   showData = function() {
     chordChart(chart, settings);
@@ -24,6 +43,9 @@ i18n.load([sgcI18nRoot, countryI18nRoot, rootI18nRoot], function() {
     .await(function(error, sgcs, countries, birthplace) {
       sgcFormatter = sgc.getFormatter(sgcs);
       countriesData = statcan_countries(countries);
+      birthplaceData = processData(birthplace.pob);
+
+      settings.data = birthplaceData.mappings;
 
       showData();
     });
