8ffb50176f5da560c401225225da6763f6ba47a4
Laurent Goderre <laurent.goderre@gmail.com>
2017-09-20 15:25:18 -0400

    Added data filtering function

--- src/demo.js
+++ src/demo.js
@@ -1,3 +1,10 @@
+const FROM_CONTINENTS = 1,
+  FROM_REGION = 2,
+  FROM_COUNTRY = 3,
+  TO_CANADA = 1,
+  TO_PT = 2,
+  TO_CMA = 3;
+
 var sgcI18nRoot = "lib/statcan_sgc/i18n/sgc/",
   countryI18nRoot = "lib/statcan_countries/i18n/",
   rootI18nRoot = "src/i18n/",
@@ -8,7 +15,18 @@ var sgcI18nRoot = "lib/statcan_sgc/i18n/sgc/",
   chart = container.append("svg")
     .attr("id", "canada_birthplace"),
   rootI18nNs = "census_birthplace",
+  canadaSgc = "01",
   settings = {
+    from: {
+      getValue: function(d) {
+        return d.pobId;
+      }
+    },
+    to: {
+      getValue: function(d) {
+        return d.sgcId;
+      }
+    },
     getPointValue: function() {
       return this.dataPoint.total;
     }
@@ -30,7 +48,33 @@ var sgcI18nRoot = "lib/statcan_sgc/i18n/sgc/",
 
     return data;
   },
+  getDataFilter = function(from, to) {
+    return function(data) {
+      return data.filter(function(d) {
+        var fromTrue = false,
+          toTrue = false,
+          toCanada = settings.to.getValue.call(settings, d) === canadaSgc,
+          toProvince = sgc.sgc.isProvince(settings.to.getValue.call(settings, d));
+        if (
+          (to === TO_CANADA && toCanada) ||
+          (to === TO_PT && toProvince) ||
+          (to === TO_CMA && !toCanada && !toProvince)
+        )
+          toTrue = true;
+
+        if (
+          (from === FROM_CONTINENTS && true) ||
+          (from === FROM_REGION && true) ||
+          (from == FROM_COUNTRY && true)
+        )
+          fromTrue = true;
+
+        return fromTrue && toTrue;
+      });
+    };
+  },
   showData = function() {
+    settings.filterData = getDataFilter(FROM_CONTINENTS, TO_CANADA);
     chordChart(chart, settings);
   },
   countriesData, birthplaceData, sgcFormatter;
